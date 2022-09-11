3eb49cc03c1a3609bc19344d84487aeec2ec8aec
Daniel Fitzick <incherre2526@gmail.com>
2022-09-11 15:14:11 -0700

    Update transformer encoder to work correctly with sequences of tokens.

--- transformer/numpy_transformer.py
+++ transformer/numpy_transformer.py
@@ -31,28 +31,34 @@ class AddAndNorm:
         post_activations = self.wrapped_layer.forward(input_activations)
         added_activations = input_activations + post_activations
 
-        input_size = added_activations.shape[0]  # Shape = (input_size, batch_size)
-        layer_mean = np.sum(added_activations, axis = 0) / input_size
-        added_activations = added_activations - layer_mean
-        layer_std = np.sqrt(np.sum(np.power(added_activations, 2), axis = 0))
-        added_activations = added_activations / (layer_std + self.nudge)
+        input_size = added_activations.shape[1] * added_activations.shape[2]   # Shape = (batch_size, embed_size, seq_len)
+        layer_mean = np.sum(added_activations, axis = (1, 2)) / input_size
+        added_activations = added_activations - layer_mean[:, None, None]
+        layer_std = np.sqrt(np.sum(np.power(added_activations, 2), axis = (1, 2)))
+        added_activations = added_activations / (layer_std[:, None, None] + self.nudge)
 
         return added_activations
 
 def attention(queries, keys, values, mask = None, mask_value = -1e9):
-    key_dim = keys.shape[0]
-    pre_softmax = (np.transpose(queries) @ keys) / np.sqrt(key_dim)
+    batch_size = keys.shape[0]
+    key_dim = keys.shape[1]
+    seq_len = keys.shape[2]
+
+    pre_softmax = (np.transpose(queries, axes=(0, 2, 1)) @ keys) / np.sqrt(key_dim)
     if mask is not None:
-        pre_softmax[mask == 0] = mask_value
-    post_softmax = np.exp(pre_softmax) / np.sum(np.exp(pre_softmax), axis=0)
-    return values @ np.transpose(post_softmax)
+        pre_softmax[:, mask == 0] = mask_value
+    post_softmax = np.exp(pre_softmax) / np.sum(
+        np.exp(pre_softmax), axis=2)[:, None, :]
+    return values @ np.transpose(post_softmax, axes=(0, 2, 1))
 
 class MultiHeadAttention:
-    def __init__(self, forward_size, num_heads, query_weights, key_weights, value_weights, final_weights):
-        assert(forward_size % num_heads == 0)
-        self.forward_size = forward_size
+    def __init__(self, embedding_size, sequence_length, num_heads, query_weights, key_weights, value_weights, final_weights):
+        self.sequence_length = sequence_length
+
+        assert(embedding_size % num_heads == 0)
+        self.embedding_size = embedding_size
         self.num_heads = num_heads
-        self.head_size = forward_size // num_heads
+        self.head_size = embedding_size // num_heads
 
         assert(len(query_weights) == num_heads)
         assert(query_weights[0].shape == (self.head_size, self.head_size))
@@ -66,32 +72,33 @@ class MultiHeadAttention:
         assert(value_weights[0].shape == (self.head_size, self.head_size))
         self.value_weights = value_weights
 
-        assert(final_weights.shape == (forward_size, forward_size))
+        assert(final_weights.shape == (embedding_size, embedding_size))
         self.final_weights = final_weights
 
     def multi_head_attention(self, queries, keys, values, mask = None):
-        num_examples = queries.shape[1]
-        query_dim = queries.shape[0]
-        key_dim = keys.shape[0]
-        value_dim = values.shape[0]
+        assert(len(queries.shape) == 3)
+        assert(queries.shape == keys.shape)
+        assert(keys.shape == values.shape)
+        assert(values.shape[1] == self.embedding_size)
+        assert(values.shape[2] == self.sequence_length)
+        num_examples = queries.shape[0]
 
-        pre_linear_output = np.zeros((self.forward_size, num_examples))
+        pre_linear_output = np.zeros((num_examples, self.embedding_size, self.sequence_length))
         for head_index in range(self.num_heads):
             lower_head_range = head_index * self.head_size
             upper_head_range = lower_head_range + self.head_size
 
-            post_linear_queries = self.query_weights[head_index] @ queries[lower_head_range:upper_head_range, :]
-            post_linear_keys = self.key_weights[head_index] @ keys[lower_head_range:upper_head_range, :]
-            post_linear_values = self.value_weights[head_index] @ values[lower_head_range:upper_head_range, :]
+            post_linear_queries = self.query_weights[head_index] @ queries[:, lower_head_range:upper_head_range, :]
+            post_linear_keys = self.key_weights[head_index] @ keys[:, lower_head_range:upper_head_range, :]
+            post_linear_values = self.value_weights[head_index] @ values[:, lower_head_range:upper_head_range, :]
 
-            pre_linear_output[lower_head_range:upper_head_range, :] = attention(
+            pre_linear_output[:, lower_head_range:upper_head_range, :] = attention(
                 post_linear_queries, post_linear_keys, post_linear_values, mask = mask)
-            
         return self.final_weights @ pre_linear_output
 
 class SelfAttention(MultiHeadAttention):
-    def __init__(self, forward_size, num_heads, query_weights, key_weights, value_weights, final_weights):
-        super().__init__(forward_size, num_heads, query_weights, key_weights, value_weights, final_weights)
+    def __init__(self, embedding_size, sequence_length, num_heads, query_weights, key_weights, value_weights, final_weights):
+        super().__init__(embedding_size, sequence_length, num_heads, query_weights, key_weights, value_weights, final_weights)
 
     def forward(self, input_activations):
         return self.multi_head_attention(input_activations, input_activations, input_activations)
@@ -108,14 +115,15 @@ def init_rand(output_width, input_width, mag = 0.1):
     return (np.random.rand(output_width, input_width) * mag * 2) - mag
 
 class Encoder:
-    def __init__(self, num_layers, layer_size, num_heads):
+    def __init__(self, num_layers, layer_size, sequence_length, num_heads):
         assert(layer_size % num_heads == 0)
         head_size = layer_size // num_heads
 
         self.layers = []
         for i in range(num_layers):
             attention_sublayer = SelfAttention(
-                layer_size, num_heads, [init_rand(head_size, head_size) for i in range(num_heads)],
+                layer_size, sequence_length, num_heads,
+                [init_rand(head_size, head_size) for i in range(num_heads)],
                 [init_rand(head_size, head_size) for i in range(num_heads)],
                 [init_rand(head_size, head_size) for i in range(num_heads)],
                 init_rand(layer_size, layer_size))
@@ -138,12 +146,35 @@ class Decoder:
 
 if __name__ == '__main__':
     attention_sublayer = SelfAttention(
-        8, 2, [np.eye(4), np.eye(4)], [np.eye(4), np.eye(4)], [np.eye(4), np.eye(4)], np.eye(8))
-    feedforward_sublayer = FeedForward(np.eye(8), np.array([[0], [0], [0], [0], [0], [0], [0], [0]]))
+        8, 4, 2, [np.eye(4), np.eye(4)], [np.eye(4), np.eye(4)], [np.eye(4), np.eye(4)], np.eye(8))
+    feedforward_sublayer = FeedForward(np.eye(8), np.array([[1], [1], [1], [1], [1], [1], [1], [1]]))
     test_encoder_layer = EncoderLayer(attention_sublayer, feedforward_sublayer)
-    test_input = np.array([[1, 3], [2, 7], [3, 8], [4, 3], [5, 9], [6, 1], [7, 4], [8, 3]])
+
+    # Shape here is (batch_size, embedding_size, sequence_length)
+    test_input = np.array([[[1, 3, 1, 0],
+                            [2, 7, 6, 0],
+                            [3, 8, 8, 0],
+                            [4, 3, 4, 0],
+                            [5, 9, 8, 0],
+                            [6, 1, 4, 0],
+                            [7, 8, 9, 0],
+                            [8, 4, 1, 0]],
+                           [[1, 3, 1, 0],
+                            [2, 7, 6, 0],
+                            [3, 8, 8, 0],
+                            [4, 3, 4, 0],
+                            [5, 9, 8, 0],
+                            [6, 1, 4, 0],
+                            [7, 8, 9, 0],
+                            [8, 4, 1, 0]]])
+
+    mask = np.array([[1, 0, 0, 0],
+                     [1, 1, 0, 0],
+                     [1, 1, 1, 0],
+                     [1, 1, 1, 1]])
+    print(attention(test_input, test_input, test_input, mask = mask))
 
     print(test_encoder_layer.forward(test_input))
 
-    test_encoder = Encoder(4, 8, 2)
+    test_encoder = Encoder(3, 8, 4, 2)
     print(test_encoder.forward(test_input))
